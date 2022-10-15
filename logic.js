a20ffb8bff6dcde97a160c28356fc0dc213562d5
githubuserds <19790262@clases.edu.sv>
2022-10-15 16:59:52 -0600

    add2

--- logic.js
+++ logic.js
@@ -1,1 +1,10 @@
+let vista_preliminar = () => {
+  let leer_img = new FileReader()
+  let id_img = document.getElementById('#img-foto')
 
+  leer_img.onload = () => {
+    if (leer_img.readyState === 2) {
+      id_img.src = leer_img.result
+    }
+  }
+}
